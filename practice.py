868243ac40c49b64577d1bfa7a37aa6a150bc27b
Tirhas <learningforcoding22@gmail.com>
2022-11-11 23:21:56 -0800

    fizz buzz function created

---
+++ practice.py
@@ -0,0 +1,13 @@
+
+def fizz_buzz(n):
+    for i in range(n):
+        if i % 3 == 0 and n % 5 == 0:
+            print("fizz_buzz")
+        elif i % 3 == 0:
+            print("buzz")
+        elif i % 5 == 0:
+            print("fiz")
+        else:
+            print(i)
+                        
+print(fizz_buzz(6))
